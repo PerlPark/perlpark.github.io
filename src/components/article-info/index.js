464cd48190ceb2097c91108e9b6166149608ee76
perlpark <jinjoo_park@naver.com>
2019-10-18 16:28:32 +0900

    font awesome 딜레이 렌더링 수정 테스트

--- src/components/article-info/index.js
+++ src/components/article-info/index.js
@@ -1,24 +1,26 @@
 import React from "react"
-import style from "./article-info.module.css"
+import style from "./article-info.module.scss"
+// import { config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCalendarAlt, faStream, faTag } from '@fortawesome/free-solid-svg-icons'
+// config.autoAddCss = false;
 
 function articleInfo(props) {
   return (
     <ul className={style.info}>
       <li className={style.info__item}>
-        <FontAwesomeIcon icon={faCalendarAlt} size="1x" className={style.info__item__icon} />
+        <FontAwesomeIcon icon={faCalendarAlt} className={style.info__item__icon} />
         {props.date}
       </li>
       
       <li className={style.info__item}>
-        <FontAwesomeIcon icon={faStream} size="1x" className={style.info__item__icon} />
+        <FontAwesomeIcon icon={faStream} className={style.info__item__icon} />
         {props.timeToRead} minute read
       </li>
 
       {props.tags && (
         <li className={style.info__item}>
-          <FontAwesomeIcon icon={faTag} size="1x" className={style.info__item__icon} />
+          <FontAwesomeIcon icon={faTag} className={style.info__item__icon} />
           <ul className={style.info__item__tags}>
             {props.tags.map((tag) =>
               <li key={tag} className={style.info__item__tags__item}>{tag}</li>
